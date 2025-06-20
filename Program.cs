8cb234bccd5fe22f35d907e6e75f70552cb77ad2
Carlos Nascimento <naano.dev@gmail.com>
2025-06-20 00:14:36 -0300

    Desafio Completo õ/

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel;
+using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Utilizando dispositivo da Apple");
+Smartphone iPhone = new Iphone(numero: "12345", modelo: "iPhone 14", imei: "1a561e651", memoria: 128);
+iPhone.Ligar();
+iPhone.ReceberLigacao();
+iPhone.InstalarAplicativo("Instagram");
+
+
+Console.WriteLine("\n");
+
+
+Console.WriteLine("Utilizando dispositivo da Nokia");
+Smartphone nokia = new Nokia(numero: "67890", modelo: "Nokia TIJOLÃO", imei: "5b848c654", memoria: 16);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
